72b5e4a4254e8e8d9f6178f3df0f1490177d8c37
Dylan Vannatter <vannattd@mail.gvsu.edu>
2021-04-25 19:34:36 -0400

    Final Submission

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -9,6 +9,13 @@ public class Enemy : MonoBehaviour
     Rigidbody2D rb;
     Animator ani;
 
+    private float timeBetweenAttack;
+    public float startTimeBetweenAttack;
+    public Transform attackPos;
+    public float attackRange;
+    public int damage;
+    public LayerMask whatIsEnemy;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,25 +23,50 @@ public class Enemy : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         ani = GetComponent<Animator>();
 
-        rb.freezeRotation = true;    
+        rb.freezeRotation = true;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(health <= 0){
+        if (health <= 0)
+        {
             ani.SetTrigger("Death");
             speed = 0;
             //Destroy(gameObject);
         }
+        if (timeBetweenAttack <= 0)
+        {
+            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemy);
+            if (enemiesToDamage.Length > 0)
+            {
+                ani.SetTrigger("Attack");
+                // Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemy);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<PlayerController>().TakeDamage(damage);
+                }
+            }
+            timeBetweenAttack = startTimeBetweenAttack;
+        }
+        else
+        {
+            timeBetweenAttack -= Time.deltaTime;
+        }
         transform.Translate(Vector2.left * speed * Time.deltaTime);
     }
 
-    public void TakeDamage(int damage){
+    public void TakeDamage(int damage)
+    {
         health -= damage;
+        ani.SetTrigger("Hurt");
         Debug.Log("OWWWWWWWWWWWWWWWWWWW");
     }
 
-
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,12 +7,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private LayerMask platform;
 
 
-private float timeBetweenAttack;
-public float startTimeBetweenAttack;
-public Transform attackPos;
-public float attackRange;
-public int damage;
-public LayerMask whatIsEnemy;
+    private float timeBetweenAttack;
+    public float startTimeBetweenAttack;
+    public Transform attackPos;
+    public float attackRange;
+    public int damage;
+    public LayerMask whatIsEnemy;
 
 
 
@@ -45,39 +45,45 @@ public LayerMask whatIsEnemy;
         rb = GetComponent<Rigidbody2D>();
         ani = GetComponent<Animator>();
         collider = GetComponent<BoxCollider2D>();
-        rb.freezeRotation = true;    
+        rb.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
-    {   
+    {
         playerPos = player.transform.position;
         float mag_velocity = Mathf.Abs(rb.velocity.x);
         ani.SetFloat("Speed", mag_velocity);
         float vert_velocity = Mathf.Abs(rb.velocity.y);
         ani.SetFloat("Falling", vert_velocity);
         Vector2 scale = player.transform.localScale;
-        if(playerPos.y <= -20){
-            Application.LoadLevel(0);        
+        if (playerPos.y <= -20)
+        {
+            Application.LoadLevel(0);
         }
-        if(Input.GetKey(KeyCode.A)){
-            Vector2 left = new Vector2(-0.05f, 0);
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector2 left = new Vector2(-1f, 0);
             rb.AddForce(left, ForceMode2D.Impulse);
             scale.x = -1;
         }
-        if(Input.GetKey(KeyCode.D)){
-            Vector2 right = new Vector2(0.05f, 0);
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector2 right = new Vector2(1f, 0);
             rb.AddForce(right, ForceMode2D.Impulse);
             scale.x = 1;
         }
-       if(Input.GetKeyDown(KeyCode.Space) && isGrounded()){
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
             Vector2 up = new Vector2(0, 10f);
             rb.AddForce(up, ForceMode2D.Impulse);
             ani.SetTrigger("Jumping");
 
         }
-        if(timeBetweenAttack <= 0){
-            if(Input.GetKey(KeyCode.E)){
+        if (timeBetweenAttack <= 0)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
                 ani.SetTrigger("Attack");
                 Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemy);
                 for (int i = 0; i < enemiesToDamage.Length; i++)
@@ -87,54 +93,87 @@ public LayerMask whatIsEnemy;
             }
             timeBetweenAttack = startTimeBetweenAttack;
         }
-        else{
+        else
+        {
             timeBetweenAttack -= Time.deltaTime;
         }
         player.transform.localScale = scale;
     }
 
-    int getHealth(){
+    int getHealth()
+    {
         return health;
     }
 
 
-    private bool isGrounded(){
+    private bool isGrounded()
+    {
         float buffer = 0.01f;
         RaycastHit2D raycastHit = Physics2D.Raycast(collider.bounds.center, Vector2.down, collider.bounds.extents.y + buffer);
 
-       Debug.Log(raycastHit.collider);
-       return raycastHit.collider != null;
+        Debug.Log(raycastHit.collider);
+        return raycastHit.collider != null;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision){
-        if (collision.transform.tag == "Enemy"){
-            if(!invulnerable){
-                if(health == 3){
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Enemy")
+        {
+            if (!invulnerable)
+            {
+                if (health == 3)
+                {
                     Destroy(live1);
                     StartCoroutine(JustHurt());
                 }
-                if(health == 2){
+                if (health == 2)
+                {
                     Destroy(live2);
                     StartCoroutine(JustHurt());
                 }
-                if(health == 1){
+                if (health == 1)
+                {
                     Destroy(live3);
-                    Application.LoadLevel(0);        
+                    Application.LoadLevel(0);
                 }
             }
         }
-     }
+    }
 
-     void OnDrawGizmosSelected(){
-         Gizmos.color = Color.red;
-         Gizmos.DrawWireSphere(attackPos.position, attackRange);
-     }
+    public void TakeDamage(int damage)
+    {
+        if (!invulnerable)
+        {
+            if (health == 3)
+            {
+                Destroy(live1);
+                StartCoroutine(JustHurt());
+            }
+            if (health == 2)
+            {
+                Destroy(live2);
+                StartCoroutine(JustHurt());
+            }
+            if (health == 1)
+            {
+                Destroy(live3);
+                Application.LoadLevel(0);
+            }
+        }
+    }
 
-    IEnumerator JustHurt(){
-     invulnerable = true;
-     Debug.Log("hit");
-     yield return new WaitForSeconds(invulTime);
-     health--;
-     invulnerable = false;
- }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+
+    IEnumerator JustHurt()
+    {
+        invulnerable = true;
+        Debug.Log("hit");
+        yield return new WaitForSeconds(invulTime);
+        health--;
+        invulnerable = false;
+    }
 }
